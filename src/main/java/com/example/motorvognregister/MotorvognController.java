a1d0e31e67ce899801a557eacb600126ba81fc09
“JarlM06” <jarl.ebbesen@outlook.com>
2024-01-08 09:55:27 +0100

    Skrev alt som skal være i MotorvognController.java; liste, PostMapping for lagring og sletting, og GetMapping for henting

--- src/main/java/com/example/motorvognregister/MotorvognController.java
+++ src/main/java/com/example/motorvognregister/MotorvognController.java
@@ -1,4 +1,33 @@
 package com.example.motorvognregister;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class MotorvognController {
+
+    // Opprette en liste som inneholder alle motorvognene
+    private final List<Motorvogn> alleMotorvogner = new ArrayList<>();
+
+    // PostMapping som lagrer motorvognen i listen med alle motorvognene
+    @PostMapping("/lagre")
+    public void lagreMotorvogn(Motorvogn innMotorvogn) {
+        alleMotorvogner.add(innMotorvogn);
+    }
+
+    // GetMapping som henter listen med alle motorvognene
+    @GetMapping("/hentAlle")
+    public List<Motorvogn> hentAlle() {
+        return alleMotorvogner;
+    }
+
+    // PostMapping som sletter alt innholdet i listen med motorvognene
+    @PostMapping("/slett")
+    public void slettMotorvogner() {
+        alleMotorvogner.clear();
+    }
 }
